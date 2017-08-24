66dd22b7e821e00ccd7080992419182f15025649
Ryan potato pants <ryanja567@gmail.com>
2017-08-24 14:55:07 -0700

    update

--- app.js
+++ app.js
@@ -3,31 +3,31 @@ var path = require('path');
 const app = express();
 const port = 8080;
 
-app.use(express.static('./views'));
+app.use(express.static('./public'));
 
 // homepage
 app.get('/', function(req, res) {
-  res.sendfile(path.resolve(__dirname + '/views/index.html'));
+  res.sendfile(path.resolve(__dirname + '/public/views/index.html'));
 });
 
 // about page
 app.get('/about', function(req, res) {
-  res.sendfile(path.resolve(__dirname + '/views/about.html'));
+  res.sendfile(path.resolve(__dirname + '/public/views/about.html'));
 });
 
 // contact page
 app.get('/contact', function(req, res) {
-  res.sendfile(path.resolve(__dirname + '/views/contact.html'));
+  res.sendfile(path.resolve(__dirname + '/public/views/contact.html'));
 });
 
 // store page
 app.get('/store', function(req, res) {
-  res.sendfile(path.resolve(__dirname + '/views/store.html'));
+  res.sendfile(path.resolve(__dirname + '/public/views/store.html'));
 });
 
 // error page
 app.get('*', function(req, res) {
-  res.sendfile(path.resolve(__dirname + '/views/404.html'));
+  res.sendfile(path.resolve(__dirname + '/public/views/404.html'));
 });
 
 app.listen(port, function () {
